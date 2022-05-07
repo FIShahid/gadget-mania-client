ad74e3a5ce557034dd5792e841a88bb7705523b7
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-08 04:03:42 +0600

    UI fixing

--- src/Pages/ManageInventory/ManageInventory.js
+++ src/Pages/ManageInventory/ManageInventory.js
@@ -1,32 +1,50 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useProduct from '../../hooks/useProduct';
 
 const ManageInventory = () => {
     const [products, setProducts] = useProduct()
-    
-    const handleDelete = id =>{
+
+    const handleDelete = id => {
         const proceed = window.confirm('Are you sure?');
-        if(proceed){
+        if (proceed) {
             const url = `https://intense-dawn-79079.herokuapp.com/inventory/${id}`;
             fetch(url, {
                 method: 'DELETE'
             })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                const remaining = products.filter(service => service._id !== id);
-                setProducts(remaining);
-            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = products.filter(service => service._id !== id);
+                    setProducts(remaining);
+                })
         }
     }
     return (
         <div>
-            <h3>Manage Inventory</h3>
+            <h3 className='text-center mt-3 mb-3'>Manage Inventory</h3>
             {
-                products.map(product=> <div key={product._id}>
-                    <h4>{product.name}</h4> <button onClick={()=>handleDelete(product._id)} className='btn btn-danger'>Delete</button>
-                    <Link to='/addItem' ><button className='btn btn-info'>Add Item</button> </Link>
+                products.map(product => <div key={product._id}>
+                    <Table striped bordered hover responsive>
+
+                       
+                        <tbody className=''>
+                            <tr>
+                                <td className='fw-bold'>{product.name}</td>
+                                <td><img style={{height:'60px'}} src={product.img} alt="" /></td>
+
+                                <td className='fw-bold'><span>Available Product: </span>{product.stock}</td>
+                                <td> <button onClick={() => handleDelete(product._id)} className='btn btn-danger px-2'>Delete</button> <Link to='/addItem' ><button className='btn btn-info'>Add Item</button> </Link>
+                                </td>
+                            </tr>
+
+
+                        </tbody>
+                    </Table>
+
+
+
 
                 </div>)
             }
